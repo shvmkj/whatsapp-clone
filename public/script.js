184beeb17a9b20807629dd52c693da3e1f58d544
shvmkj <sheevam1101@gmail.com>
2021-07-23 04:03:03 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,25 @@
+let socket = io()
+$('#loginBox').show()
+$('#chatBox').hide()
+$('#loginBtn').click(()=>{
+    socket.emit("login",{
+        username : $('#username').val(),
+        password : $('#password').val()
+    })
+})
+socket.on("welcome",()=>{
+    $('#loginBox').hide()
+    $('#chatBox').show()
+})
+$('#sendMessage').click(()=>{
+    socket.emit("message",{
+        to : $('#receiver').val(),
+        msg    : $('#msg').val()
+    })
+})
+socket.on("msg_rcvd",(data)=>{
+    $('#ulMsgs').append($('<li>').text(data.msg))
+})
+socket.on("login_failed",()=>{
+    window.alert("Username or Password Wrong")
+})
---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const socketio = require('socket.io')
+const express = require('express')
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+const socketMap = {}
+app.use('/',express.static(__dirname+'/public'))
+let users = {
+    shivam : "mynameisshivam",
+}
+io.on('connection',(socket)=>{
+    function login(socket,username){
+        socket.join(username);
+        socket.emit('welcome')
+        socketMap[socket.id]=username
+        console.log(socketMap)
+    }
+    socket.on('login',(data)=>{
+        if(users[data.username]){
+            if(users[data.username]==data.password){
+                login(socket,data.username)
+            }else{
+                socket.emit('login_failed')
+            }    
+        }else{
+            users[data.username]=data.password
+            login(socket,data.username)
+        }
+        
+        
+        console.log(data)
+    })
+    socket.on('message',(data)=>{
+        if(data.to){
+            console.log(data.to)
+            io.to(data.to).emit("msg_rcvd",data)
+        }else{
+            socket.broadcast.emit('msg_rcvd',data)
+        }
+        
+    })
+    
+})
+server.listen(4444,()=>{
+    console.log("server is running on http://localhost:4444")
+})
